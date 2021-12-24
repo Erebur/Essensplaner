1574c7ad41b5859ededba13353f0cfe0bdcda2d5
erebur <simon.web999@gmail.com>
2021-12-24 11:57:14 +0100

    Server: Reformated index.js

--- server/index.js
+++ server/index.js
@@ -7,42 +7,51 @@ const app = express();
 const sqlite3 = require('sqlite3');
 
 const PORT = process.env.PORT || 3001;
-app.use("/", router);
+
 
 let test = "Help";
 
+let db = DB();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+API();
 
-router.get('/', (req, res) => {
-    res.sendfile("index.html");
-})
+function DB() {
+    return new sqlite3.Database('db/login.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log('Connected to the database.');
+        }
+    });
+}
 
-app.post("/api/login", (req, res) => {
+function API() {
 
-})
+    app.use("/", router);
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json())
+
+    router.get('/', (req, res) => {
+        res.sendfile("index.html");
+    });
+
+    app.post("/api/login", (req, res) => {
+    });
 
 
-app.get("/api/did", (req, res) => {
-    if (req)
-        res.json({ message: test })
-    console.log(res)
-})
+    app.get("/api/did", (req, res) => {
+        if (req)
+            res.json({ message: test });
+        console.log(res);
+    });
 
-app.post("/api/put", (req, res) => {
-    console.log(req.body)
-    res.json({ message: "hello" })
-})
+    app.post("/api/put", (req, res) => {
+        console.log(req.body);
+        res.json({ message: "hello" });
+    });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+    });
+}
 
-let db = new sqlite3.Database('db/login.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log('Connected to the database.');
-    }
-});
